4fc152fbbe641ca724457419f91eb726c3f410e9
Nelson M <mwinel@live.com>
2022-11-18 10:46:20 +0300

    Add accent color to blog title

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -6,11 +6,11 @@ const BlogPost = ({ title, summary, slug, publishedAt }) => {
       <a className="w-full">
         <div className="mb-4 w-full">
           <div className="flex flex-col md:flex-row justify-between">
-            <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+            <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-blue-500 hover:underline dark:text-gray-100">
               {title}
             </h4>
           </div>
-          <p className="text-base lg:text-lg text-gray-800 dark:text-gray-400">
+          <p className="text-base lg:text-lg text-gray-600 dark:text-gray-400">
             {summary}
           </p>
           <div className="flex items-center">
