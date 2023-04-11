913477aac54558fb41aa0b7b21913cb0cee3ed96
Nelson M <mwinel@live.com>
2023-04-11 09:29:30 +0300

    Update css font

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import { Inter, JetBrains_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 const jetbrains_mono = JetBrains_Mono({ subsets: ["latin"] });
 
 const MyApp = ({ Component, pageProps }) => {
